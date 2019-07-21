ae02128bd7fad0880ffa90c30371dae3e3a4d8a0
wesaphzt <mrazaca@pm.me>
2019-07-21 20:30:51 +0100

    implement randomize location

--- app/src/main/java/com/wesaphzt/privatelocation/fragments/FragmentSettings.java
+++ app/src/main/java/com/wesaphzt/privatelocation/fragments/FragmentSettings.java
@@ -17,14 +17,21 @@ import com.wesaphzt.privatelocation.R;
 
 public class FragmentSettings extends PreferenceFragmentCompat {
 
-    private SharedPreferences prefs;
+    private Context context;
+
+    private CheckBoxPreference cbRandomize;
+
+    private SharedPreferences sharedPreferences;
+    private SharedPreferences.OnSharedPreferenceChangeListener sharedPreferenceChangeListener;
 
     @Override
     public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
         addPreferencesFromResource(R.xml.preferences);
 
         setHasOptionsMenu(true);
-        Context context = getContext();
+        context = getContext();
+
+        sharedPreferences = PreferenceManager.getDefaultSharedPreferences(context);
 
         //this static call will reset default values only on the first ever read
         PreferenceManager.setDefaultValues(context, R.xml.preferences, false);
@@ -38,6 +45,24 @@ public class FragmentSettings extends PreferenceFragmentCompat {
     }
 
     @Override
+    public void onResume() {
+        super.onResume();
+        sharedPreferences.registerOnSharedPreferenceChangeListener(sharedPreferenceChangeListener);
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        sharedPreferences.unregisterOnSharedPreferenceChangeListener(sharedPreferenceChangeListener);
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        sharedPreferences.unregisterOnSharedPreferenceChangeListener(sharedPreferenceChangeListener);
+    }
+
+    @Override
     public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
         //set title
@@ -45,6 +70,16 @@ public class FragmentSettings extends PreferenceFragmentCompat {
 
         //background color
         view.setBackgroundColor(getResources().getColor(R.color.white));
+
+        sharedPreferenceChangeListener = new SharedPreferences.OnSharedPreferenceChangeListener() {
+            @Override
+            public void onSharedPreferenceChanged(SharedPreferences prefs, String key) {
+                if(key.equals("RANDOMIZE_LOCATION") && cbRandomize.isChecked()) {
+                    Toast.makeText(context, getString(R.string.settings_randomize_toast), Toast.LENGTH_LONG).show();
+                }
+            }
+        };
+        sharedPreferences.registerOnSharedPreferenceChangeListener(sharedPreferenceChangeListener);
     }
 
     @Override
